c6701e9f58edf91a71aeff325a0bbb6fcd38f5c4
Oleg <brezitskyyoleg@gmail.com>
2021-06-26 00:49:23 +0200

    Deleted relations

--- custom/db_data.py
+++ custom/db_data.py
@@ -6,18 +6,3 @@ descriptions = [
     '''Get bacterium's 625 genome length.''',
     '''Select all diseases that cause the coccus bacteria.''',
 ]
-
-# Relation: [destination, origin(s), type]
-# Types of relationships:
-# 1 -> 1:1, 2 -> 1:N, 3 -> N:M
-relations = [
-    ['EMPLEADO', 'DEPARTAMENTO', 2],
-    ['DEPARTAMENTO', 'EMPLEADO', 2],
-    ['PRODUCTO', 'PROYECTO', 2],
-    ['PROYECTO', 'DEPARTAMENTO', 2],
-    ['EVENTO', 'EMPRESA_EXT', 2],
-    ['UNIVERSIDAD', 'EMPRESA_EXT', 2],
-    ['BECARIO', 'UNIVERSIDAD', 2],
-    ['AFILIADO_A', ['DEPARTAMENTO', 'EMPRESA_EXT'], 3],
-    ['PROVEE_A', ['PROYECTO', 'EMPRESA_EXT'], 3],
-]
